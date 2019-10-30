f748fdef820bc938219fb04ec70cae018fa19f84
Alumno <Alumno@PC25>
2019-10-30 11:41:35 -0600

    chipres

--- Actividad30/Actividad30/Actividad30.cpp
+++ Actividad30/Actividad30/Actividad30.cpp
@@ -10,15 +10,13 @@ int main()
 {
 	int numero[5]; //Array o vector de 5 números
 	int suma; //Un entero que guardará la suma
-	numero[0] = 200;
-	numero[1] = 150;
-	numero[2] = 100;
-	numero[3] = -50;
-	numero[4] = 300;
-	suma = numero[0] + numero[1] + numero[2] + numero[3] + numero[4];
+	for (int i = 0; i <= 5; i++)
+	{
+		cout << "Introduce un numero: ";
+		cin >> numero[i];
+		suma = +numero[i];
+	}
 	cout << "La suma es: " << suma;
 	return 0;
-
 }
-
 
